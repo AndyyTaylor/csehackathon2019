6358217343267fc758807c83d5fd90f499bb6f87
Andy <andy@ataylor.xy>
2019-07-27 12:29:19 +1000

    Init 2 form states

--- web/src/App.js
+++ web/src/App.js
@@ -3,23 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/Header';
+import ApplianceInputForm from './components/ApplianceInputForm';
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
+
+            <ApplianceInputForm detail />
         </div>
     );
 }
---
+++ web/src/components/ApplianceInputForm.js
@@ -0,0 +1,113 @@
+
+import React, { Component } from 'react';
+
+
+class ApplianceInputForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            energyCost: undefined,
+            company: undefined,
+            model: undefined,
+            type: undefined,
+            consumption: undefined,
+            length: undefined,
+            width: undefined,
+            height: undefined
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e, key) {
+        const newState = JSON.parse(JSON.stringify(this.state));
+
+        newState[key] = e.target.value;
+
+        this.setState(newState);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        console.log('submitted');
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <form onSubmit={ this.handleSubmit }>
+
+                <div className="appliance_container">
+                    <div className="form-group row">
+                        <label className="col-sm-4 col-form-label text-justify">Energy Cost</label>
+                        <div className="col-sm-8">
+                            <input type="text" className="form-control" value={ this.state.energyCost } onChange={ (e) => this.handleChange(e, 'energyCost') } />
+                            <small className="form-text text-muted">If you don't know it, leave it blank :))</small>
+                        </div>
+                    </div>
+
+                    <div className="form-group row">
+                        <label className="col-sm-4 col-form-label text-justify">Type</label>
+                        <div className="col-sm-8">
+                            <select className="form-control">
+                                <option value="fridge">Fridge</option>
+                                <option value="dishwasher">Dishwasher</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    {   !this.props.detail &&
+                        <>
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-form-label text-justify">Company</label>
+                            <div className="col-sm-8">
+                                <input type="text" className="form-control" value={ this.state.company } onChange={ (e) => this.handleChange(e, 'company') } />
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-form-label text-justify">Model</label>
+                            <div className="col-sm-8">
+                                <input type="text" className="form-control" value={ this.state.model } onChange={ (e) => this.handleChange(e, 'model') } />
+                            </div>
+                        </div>
+                        </>
+                    }
+
+                    { this.props.detail &&
+                        <>
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-form-label text-justify">Energy Consumption</label>
+                            <div className="col-sm-8">
+                                <input type="text" className="form-control" value={ this.state.consumption } onChange={ (e) => this.handleChange(e, 'consumption') } />
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-form-label text-justify">Length</label>
+                            <div className="col-sm-8">
+                                <input type="number" className="form-control" value={ this.state.length } onChange={ (e) => this.handleChange(e, 'length') } />
+                            </div>
+                        </div>
+                        </>
+                    }
+
+                    {/* <p className="text-justify">Type </p>
+                    <select value={ this.state.form.appliances[0].type } onChange={ (e) => this.handleApplianceChange(e, 0, 'type') }>
+                        <option value="fridge">Fridge</option>
+                        <option value="dishwasher">Dishwasher</option>
+                    </select> */}
+
+                    <input type="submit" value="Submit" className="btn btn-primary float-right"/>
+                </div>
+            </form>
+        )
+    }
+
+}
+
+export default ApplianceInputForm;
