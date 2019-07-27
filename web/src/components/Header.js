e03e6ae392fe6cbf33b870bff14f5ac80281688b
Andy <andy@ataylor.xy>
2019-07-27 11:01:35 +1000

    Added header framework

---
+++ web/src/components/Header.js
@@ -0,0 +1,23 @@
+
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { };
+    }
+
+    render() {
+        return (
+            <p>
+                // Carey fill this out and make it look like an actual header on a proper website :))
+            </p>
+        )
+    }
+
+}
+
+export default Header;
