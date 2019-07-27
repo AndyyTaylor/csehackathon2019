e9d3b45cf5005b3ef4c41114d258655cbd0b2c79
Andy <andy@ataylor.xy>
2019-07-27 21:12:18 +1000

    More work on detail info section

--- web/src/components/DetailView.js
+++ web/src/components/DetailView.js
@@ -1,4 +1,7 @@
+
 import React, { Component } from 'react';
+
+import { Bar } from 'react-chartjs-2';
 
 class DetailView extends Component {
 
@@ -14,10 +17,28 @@ class DetailView extends Component {
               <div className="detail-img-container">
                 <img className="rounded detail-image" src="https://www.appliancesonline.com.au/public/images/product/ktb2302wa/external/Kelvinator-KTB2302WA-231L-Top-Mount-Fridge-Hero-Image-high.jpeg" />
               </div>
-              <div>
-                  <h3>My Epic Fridge Title</h3>
-                  <div>$1,999</div>
-                  <div>600mm x 546mm x 300mm (L x W x H)</div>
+              <div className="detail-info">
+                    <h3>My Epic Fridge Title</h3>
+                    <div>$1,999</div>
+                    <div>600mm x 546mm x 300mm (L x W x H)</div>
+
+                    {/* https://www.chartjs.org/docs/latest/ */}
+                    {/* https://www.npmjs.com/package/react-chartjs-2 */}
+                    <Bar width={100} height={50} data={{
+                            labels: ['Old kwh', 'Old co2', 'New kwh', 'New co2'],
+                            datasets: [{
+                                label: 'Energy',
+                                data: [40, 50, 10, 20]
+                            }]
+                        }} options={{
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: true
+                                    }
+                                }]
+                            }
+                        }}/>
 
                   <a href="#" className="btn btn-primary">Buy Appliance</a>
               </div>
